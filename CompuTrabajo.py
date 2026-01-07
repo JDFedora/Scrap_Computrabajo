172b1e5f8404a3600b70710c3c1bd086ac90ab8d
JDWindows <juandavidrioscanon@hotmail.com>
2026-01-06 20:37:50 -0500

    Mejora en el tipo de busqueda dentro de la web

--- CompuTrabajo.py
+++ CompuTrabajo.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 url_semilla ="https://co.computrabajo.com/"
 
+
 Nombre_Trabajo =[]
 Nombre_Empresa =[]
 Ubicacion =[]
@@ -14,7 +15,7 @@ Fecha_Publicacion =[]
 Descripcion_Trabajo =[]
 Salario_Trabajo =[]
 
-url= "https://co.computrabajo.com/trabajo-de-analista-de-informacion-en-bogota-dc"
+url= "https://co.computrabajo.com/trabajo-de-analista-de-datos-en-bogota-dc-ciudad"
 
 #Idea es aplicar y enviar la hoja de vida de los trabajos nuevos automaticamente
 #si se cumplen las condiciones de: salario, nombre del cargo.
@@ -24,15 +25,43 @@ with sync_playwright() as p:
     page.goto(url)
     page.expect_request_finished
     soup = bs4.BeautifulSoup(page.content(), 'html.parser')
-    trabajos = soup.find_all('a', class_='js-o-link fc_base')
-    
-    for trabajo in trabajos:
-        Nombre_Trabajo.append(trabajo.text.strip())
+    #se toma todas las propuestas de la pagina
+    prop = soup.find_all('article', class_='box_offer')
+
+    #se evalua una por una la informacion
+    for p in prop:
+        #nombre del trabajo
+        nom_t = p.find('a', class_='js-o-link fc_base')
+        Nombre_Trabajo.append(nom_t.text.strip())
+        #nombre de la empresa ( no siempre esta disponible)
+        try:
+            nom_e = p.find('a', class_='fc_base t_ellipsis')
+            Nombre_Empresa.append(nom_e.text.strip())
+        except:
+            Nombre_Empresa.append('No disponible - Importante empresa del sector')
+        #ciudad
+        try:
+            ubi = p.find('span', class_='mr10')
+            Ubicacion.append(ubi.text.strip()) 
+        except:
+            Ubicacion.append('No disponible')
+        #fecha de publicacion
+        try:   
+            fecha = p.find('p', class_='fs13 fc_aux mt15')
+            Fecha_Publicacion.append(fecha.text.strip())
+        except:
+            Fecha_Publicacion.append('No disponible')
+        #salario
+        try:
+            sal = p.find('span', class_='dIB mr10')  
+            Salario_Trabajo.append(sal.text.strip())
+        except:
+            Salario_Trabajo.append('No disponible')
 
     browser.close()
 
 
 
 
-print(Nombre_Trabajo)
-print(len(Nombre_Trabajo))
+#print(Nombre_Trabajo)
+print(len(Nombre_Trabajo),len(Nombre_Empresa),len(Ubicacion),len(Fecha_Publicacion),len(Salario_Trabajo))
