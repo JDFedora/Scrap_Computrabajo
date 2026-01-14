87dc831725d7036a17d6908150951328f6f72343
JDWindows <juandavidrioscanon@hotmail.com>
2026-01-14 17:54:05 -0500

    Implementado busqueda en varias paginas, informacion descargada en excel

--- CompuTrabajo.py
+++ CompuTrabajo.py
@@ -14,14 +14,12 @@ Ubicacion =[]
 Fecha_Publicacion =[]
 Descripcion_Trabajo =[]
 Salario_Trabajo =[]
+Links_Trabajo =[]
 
 url= "https://co.computrabajo.com/trabajo-de-analista-de-datos-en-bogota-dc-ciudad"
+url_bucle = url+'?p=2'
 
-#Idea es aplicar y enviar la hoja de vida de los trabajos nuevos automaticamente
-#si se cumplen las condiciones de: salario, nombre del cargo.
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
+def Scrap_Trabajos(url):
     page.goto(url)
     page.expect_request_finished
     soup = bs4.BeautifulSoup(page.content(), 'html.parser')
@@ -57,11 +55,34 @@ with sync_playwright() as p:
             Salario_Trabajo.append(sal.text.strip())
         except:
             Salario_Trabajo.append('No disponible')
+        #link del trabajo
+        try:
+            link = p.find('a', class_='js-o-link fc_base')
+            Links_Trabajo.append(url_semilla + link['href'])
+        except:
+            Links_Trabajo.append('No disponible')
+
+#Idea es aplicar y enviar la hoja de vida de los trabajos nuevos automaticamente
+#si se cumplen las condiciones de: salario, nombre del cargo.
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    Scrap_Trabajos(url)
+    for i in range(2,11):
+        url_pagina = url+'?p='+str(i)
+        Scrap_Trabajos(url_pagina)
 
     browser.close()
 
 
-
+df = pd.DataFrame({'Nombre_Trabajo':Nombre_Trabajo,
+                   'Nombre_Empresa':Nombre_Empresa,
+                   'Ubicacion':Ubicacion,
+                   'Fecha_Publicacion':Fecha_Publicacion,
+                   'Salario_Trabajo':Salario_Trabajo,
+                   'Link_Trabajo':Links_Trabajo
+                   })
+df.to_excel('./CompuTrabajo.xlsx', index=False)
 
 #print(Nombre_Trabajo)
 print(len(Nombre_Trabajo),len(Nombre_Empresa),len(Ubicacion),len(Fecha_Publicacion),len(Salario_Trabajo))
